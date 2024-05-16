f9feea167a8b5b741c6ae6fde9b8f1410f7fb970
Eashika Naznin <eashikanaznin@Eashikas-MacBook-Pro.local>
2024-05-16 09:57:34 +0600

    Usercard practice test

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,18 @@
 import React from "react"
-import { TodoList } from "./TodoList"
-import { TodoListClass } from "./TodoListClass"
-import { Name } from "./Name"
-import { TodoChecklist } from "./TodoChecklist"
-import "./style.css";
+import { UserData } from "./UserData"
+
+import "./user.css";
 import user from "./user.json";
-import img from "./sample.png";
 
 
 function App() {
 
-  // // JXS is XML version of JS
-  // // camelcased except data ans aria attr
-  // // wrapped by {} is JS code
-
-
-  // // return array / string
-  // // return [1,2,3]
-
-  // const myCustomlbl = <label htmlFor="inputId">TEST</label>
-  // return  (
-  // // <h1 id="5" className="pink" style={{backgroundColor: "blue"}}>
-  // //  {2+2}
-  // // </h1>
-  
-  // // Practice task 
-  // <div className="large" id="largeDiv">
-  //   {myCustomlbl}
-  //   <input id="inputId" type="number" defaultValue="3" />
-  // </div>
-
-  // )
-
-  // return React.createElement("h1", {'id': "5"}, "Hello World") 
-
-
+ 
   return (
-    <div>
-      {/* <h1>Todo List</h1> */}
-      <h1>Todo List</h1>
-
-      <Name name="My Cus name 1" age={50} isAdult={true} />
-      <Name name="My Cus name 2" isAdult={false} />
-
-      <AnotherName/>
-
-      <TodoList>
-        <span>child name</span>
-      </TodoList>
-
-      <TodoListClass fruitTitle = "Mango">
-        <li>Apple</li>
-      </TodoListClass>
-
-      {/* checkbox example */}
-      <TodoChecklist isComplete="true">
-        Jane
-      </TodoChecklist>
-
-      {/*  Rendering json */}
-      <div>{JSON.stringify(user)}</div>
-
-      {/*  Rendering image */}
-      <div><img src={img}/></div>
-      
-    </div>
+    <UserData name={sarah} age={20} phoneNumber={2222} address={wewewe} />
 
   )
   
-}
 
-// Keeping component in the same file
-class AnotherName extends React.Component {
-  render(){
-    return <b> Naznin</b>
-  }
 }
 export default App
