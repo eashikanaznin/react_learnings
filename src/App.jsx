8302901d12fa4498175c4c5a28c56314e47ba500
Eashika Naznin <eashikanaznin@Eashikas-MacBook-Pro.local>
2024-05-28 14:00:18 +0600

    advanced todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,55 @@ import { useState, useReducer } from "react"
 import "./styles.css"
 import { TodoItem } from "./TodoItem"
 
-const ACTIONS = {
-  ADD_TODO: "ADD_TODO",
-  DELETE_TODO: "DELETE_TODO",
-  FILTER_TODO: "FILTER_TODO",
+
+const ACTION = {
+  "ADD_NEW_TODO" : "ADD_NEW_TODO",
+  "DELETE_TODO" : "DELETE_TODO"
 }
+function reducer( myTodos, action){
+  switch(action.type){
+    case "ADD_NEW_TODO":
+    if (action.payload.value === "") return
 
-function App() {
-  const [myTodo, dispatch] = useReducer(reducer, InitCount);
-  const [newTodoName, setNewTodoName] = useState("")
-  const [todos, setTodos] = useState([])
-
-  function addNewTodo() {
-    if (newTodoName === "") return
-
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { name: newTodoName, completed: false, id: crypto.randomUUID() },
-      ]
-    })
+    // setTodos(currentTodos => {
+    //   return [
+    //     ...currentTodos,
+    //     { name: newTodoName, completed: false, id: crypto.randomUUID() },
+    //   ]
+    // })
+    // setNewTodoName("")
+    return [ ...myTodos, { name: action.payload.value, completed: false, id: crypto.randomUUID() } ]
     setNewTodoName("")
   }
 
+}
+
+function App({ initVal = [] }) {
+  const [newTodoName, setNewTodoName] = useState("")
+  // const [todos, setTodos] = useState([])
+  const [myTodos, dispatch] = useReducer( reducer, initVal)
+
+  // function addNewTodo() {
+  //   if (newTodoName === "") return
+
+  //   setTodos(currentTodos => {
+  //     return [
+  //       ...currentTodos,
+  //       { name: newTodoName, completed: false, id: crypto.randomUUID() },
+  //     ]
+  //   })
+  //   setNewTodoName("")
+  // }
+
+  // function toggleTodo(todoId, completed) {
+  //   setTodos(currentTodos => {
+  //     return currentTodos.map(todo => {
+  //       if (todo.id === todoId) return { ...todo, completed }
+
+  //       return todo
+  //     })
+  //   })
+  // }
   function toggleTodo(todoId, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
@@ -44,7 +70,7 @@ function App() {
   return (
     <>
       <ul id="list">
-        {todos.map(todo => {
+        {myTodos.map(todo => {
           return (
             <TodoItem
               key={todo.id}
@@ -65,7 +91,7 @@ function App() {
           onChange={e => setNewTodoName(e.target.value)}
         />
         {/* <button onClick={addNewTodo}>Add Todo</button> */}
-        <button onClick={() => ACTIONS.ADD_TODO}>Add Todo</button>
+        <button onClick={ ()=> dispatch({ type: ACTION.ADD_NEW_TODO, payload: { value : newTodoName} })}>Add Todo</button>
       </div>
     </>
   )
