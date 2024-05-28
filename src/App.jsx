9ef0a0db3abc671c2f75336d6d2ee8b8d2faf752
Eashika Naznin <eashikanaznin@Eashikas-MacBook-Pro.local>
2024-05-28 12:34:09 +0600

    advancd todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,74 @@
-import "./style.css";
-import { StateForm } from "./StateForm"
-import { RefForm } from "./RefForm";
+import { useState, useReducer } from "react"
+import "./styles.css"
+import { TodoItem } from "./TodoItem"
 
-function App() {
-
-   
-  return (
-    // <StateForm/>
-    <RefForm/>
-  )
-  
+const ACTIONS = {
+  ADD_TODO: "ADD_TODO",
+  DELETE_TODO: "DELETE_TODO",
+  FILTER_TODO: "FILTER_TODO",
 }
 
-export default App;
+function App() {
+  const [myTodo, dispatch] = useReducer(reducer, InitCount);
+  const [newTodoName, setNewTodoName] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function addNewTodo() {
+    if (newTodoName === "") return
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
+      ]
+    })
+    setNewTodoName("")
+  }
+
+  function toggleTodo(todoId, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === todoId) return { ...todo, completed }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(todoId) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== todoId)
+    })
+  }
+
+  return (
+    <>
+      <ul id="list">
+        {todos.map(todo => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          )
+        })}
+      </ul>
+
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">New Todo</label>
+        <input
+          type="text"
+          id="todo-input"
+          value={newTodoName}
+          onChange={e => setNewTodoName(e.target.value)}
+        />
+        {/* <button onClick={addNewTodo}>Add Todo</button> */}
+        <button onClick={() => ACTIONS.ADD_TODO}>Add Todo</button>
+      </div>
+    </>
+  )
+}
+
+export default App
